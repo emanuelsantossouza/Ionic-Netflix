a09b2a3aac88c952ec8cc92af90166ba70365e73
emanuel santoss <emanuelsantoss42@gmail.com>
2023-03-14 11:49:13 -0300

    Desevolvendo os stories

--- code/src/app/home/home.page.ts
+++ code/src/app/home/home.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { HttpClient} from '@angular/common/http'
+import { Observable } from 'rxjs';
+import { IFilmeApi, IListaFilme } from './IFilmeApi';
 
 @Component({
   selector: 'app-home',
@@ -7,36 +9,40 @@ import { HttpClient} from '@angular/common/http'
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  public movies: Array<any> = new Array<any>();
-  public stories: Array<any> = new Array<any>();
+  // public movies: Array<any> = new Array<any>();
+  // public stories: Array<any> = new Array<any>();
 
-  langua = 'pt-BR';
-  regiao = 'BR';
+  // langua = 'pt-BR';
+  // regiao = 'BR';
 
-  private apiUrl = 'https://api.themoviedb.org/3/'
-  private Key = '?api_key=97e6fa405cc6395cc947aad2e175d1d4'
+  // private apiUrl = 'https://api.themoviedb.org/3/'
+  // private Key = '?api_key=97e6fa405cc6395cc947aad2e175d1d4'
 
+
+
+
+  // ionViewOldEnter() {
+  //   this.loadStories();
+  //   this.loadMovies();
+  // }
+
+  // loadStories(): Observable<IFilmeApi> {
+  //   const url = `https://api.themoviedb.org/3/tv/airing_today?api_key=97e6fa405cc6395cc947aad2e175d1d4&language=pt-BR`;
+
+
+  //   return this.http.get<IFilmeApi>(`${url} `);
+
+  // }
+
+  // loadMovies() {
+  //   this.http.get('https://www.omdbapi.com/?apikey=3a056eae&s=love$page=1').subscribe(data => {
+  //     const response: any = data;
+
+  //     this.movies = response.Search;
+  //   });
+  // }
+
+  public slidesOptions: any = { slidesPerView: 3, spaceBetween: 10 };
 
   constructor(private http: HttpClient) { }
-
-  ionViewOldEnter() {
-    this.loadStories();
-    this.loadMovies();
-  }
-
-  loadStories() {
-    this.http.get("https://www.omdbapi.com/?apikey=3a056eae&s=net$page=1").subscribe(data => {
-      const response: any = data;
-
-      this.stories = response.Search;
-    });
-  }
-
-  loadMovies() {
-    this.http.get('https://www.omdbapi.com/?apikey=3a056eae&s=love$page=1').subscribe(data => {
-      const response: any = data;
-
-      this.movies = response.Search;
-    });
-  }
 }
